2d7ceecac176be8f6f470885c3535905f5d58d9e
ArchanaManju <archanahm2021@gmail.com>
2022-03-15 22:40:04 +0000

    added bootstarp

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Learn React
-      <p>
-        <a
-          href="https://github.com/ArchanaManju/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source Code
-        </a>
-      </p>
+    <div className="App ">
+      <div className="container">
+        <h1>Learn React </h1>
+        <button className="btn btn-primary">hello</button>
+        <footer>
+          <a
+            href="https://github.com/ArchanaManju/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source Code
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
