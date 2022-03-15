1ac0fed95736ba11ea764abf31585ee0520d31fa
ArchanaManju <archanahm2021@gmail.com>
2022-03-15 22:23:52 +0000

    github link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App">
+      Learn React
+      <p>
+        <a
+          href="https://github.com/ArchanaManju/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source Code
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
